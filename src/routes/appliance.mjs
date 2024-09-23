8d81457c90862443efce4b387e4401ea7c91ad6b
김은옥 <Summer@192-168-1-101.tpgi.com.au>
2024-09-23 23:13:36 +1000

    Update <energy-project-class_Eunok_Kim_452856230> with recent changes

--- src/routes/appliance.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { ApplianceController } from "../controllers/ApplianceController.mjs"
-
-const applianceRoutes = express.Router()
-
-// applianceRoutes.get("/", ApplianceController.viewApplianceList)
-applianceRoutes.get("/applianceRoutes", ApplianceController.getApplianceListJSON)
-
-export default applianceRoutes
