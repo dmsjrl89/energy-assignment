0eabbf8599a58c59f598a1e3ab37c841a556b648
김은옥 <Summer@MacBook-Air-3.local>
2024-09-26 15:45:38 +1000

    new version with graph

--- src/controllers/NationalSourcesController.mjs
+++
@@ -1,9 +0,0 @@
-
-import { NationalSourceModel } from "../models/NationalSourceModel.mjs"
-
-export class NationalSourcesController {
-    
-  static getStateListJSON(req, res) {
-    const nationalSource = NationalSourceModel.getAll()
-    res.status(200).json(nationalSource)
-} }
--- src/routes/locationStatics.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { NationalSourcesController } from "../controllers/NationalSourcesController.mjs"
-
-
-const locationStaticsRoutes = express.Router()
-
-locationStaticsRoutes.get("/locationStaticsRoutes", NationalSourcesController.getStateListJSON)
-
-export default locationStaticsRoutes
